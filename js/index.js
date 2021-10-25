88a4f5e46956175aea21e9b43c40529ab2ec856a
Claire List <claire.list2501@gmail.com>
2021-10-25 14:29:26 -0600

    two of ten events added

--- js/index.js
+++ js/index.js
@@ -1,2 +1,26 @@
 // Your code goes here
 //AND SO WE MEET AGAIN!
+
+//first let's select some stuff
+let alleLinks = document.querySelectorAll('nav a');
+ console.log(alleLinks);
+
+ let homeLink = document.querySelectorAll('nav a')[0]; //the 'home' link
+// //on click, change link text to 'CLICKED!'
+console.log(homeLink);
+
+homeLink.addEventListener('click',function(event){
+event.target.textContent = 'CLICKED!';
+event.preventDefault();
+});
+
+//on 'escape' key, change text back to default
+
+function escapeClicked(event){
+    if(event.key==='Escape'){ //must be event.key or key will be undefined!
+        homeLink.textContent='Home';
+    }
+}
+
+document.addEventListener('keydown',escapeClicked);
+
