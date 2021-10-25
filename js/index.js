4334f235e9d8573d3f9751ed0e9cc0c0a05dadcc
Claire List <claire.list2501@gmail.com>
2021-10-25 14:57:41 -0600

    four of ten added

--- js/index.js
+++ js/index.js
@@ -19,8 +19,45 @@ event.preventDefault();
 function escapeClicked(event){
     if(event.key==='Escape'){ //must be event.key or key will be undefined!
         homeLink.textContent='Home';
+       
     }
 }
 
 document.addEventListener('keydown',escapeClicked);
 
+////END HOME LINK STUFF
+
+//lets on mouseover change the color of the links
+let aboutLink = document.querySelectorAll('nav a')[1];
+
+aboutLink.addEventListener('mouseenter',function(event){
+    event.target.style.color = 'red';
+});
+aboutLink.addEventListener('mouseout',function(event){
+    event.target.style.color='black';
+});
+// 
+// console.log(aboutLink);
+
+//on dblclick, increase counter under h1 by 1
+//TO ADD:: ON HITTING 'ESCAPE', RESET COUNTER
+
+let header = document.querySelector('h1');
+console.log(header);
+
+let count=0;
+
+header.addEventListener('dblclick',function(event){
+    
+    event.target.textContent=`Fun Bus: ${count++}`;
+});
+
+function escapeCount(event){
+    if(event.key==='Escape'){ //must be event.key or key will be undefined!
+        header.textContent='Fun Bus';
+        count=0;
+       
+    }
+}
+
+document.addEventListener('keydown',escapeCount);
